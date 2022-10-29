34cfdf78caf1eb502715b71b951ab97500074291
Bas Man <bas.man.developer@gmail.com>
2022-10-29 19:25:57 +0100

    update cod eoutput of rspec

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Coding along with Odin Project
 
 Been doing TDD with rspec for a few weeks with Made Tech trying some different approaches
+
+## New info
+- Learnt about `--format documentation` in the `.rspec` folder
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -11,25 +11,25 @@ describe Calculator do
       expect(calculator.add(2, 5, 7)).to eq(14)
     end
   end
-  
+
   describe '#multiply' do
     it 'returns two numbes multiplied' do
       expect(calculator.multiply(3, 4)).to eq(12)
     end
   end
-  
+
   describe '#multiply' do
     it 'returns the first number minus the second number' do
       expect(calculator.subtract(5, 2)).to eq(3)
     end
   end
-  
+
   describe '#divide' do
     it 'returns the first number divided by the second' do
       expect(calculator.divide(12, 4)).to eq(3)
     end
     it 'can avoid ZeroDivisionError' do
-      expect(calculator.divide(12,0)).to eq('Divider cannot be zero')
+      expect(calculator.divide(12, 0)).to eq('Divider cannot be zero')
     end
   end
 end
