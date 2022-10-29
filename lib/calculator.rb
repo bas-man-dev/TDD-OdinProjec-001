1724b265d0c29c5ff4202d9ff46f1f704a95c7bd
Bas Man <bas.man.developer@gmail.com>
2022-10-29 15:22:10 +0100

    refactor

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,5 +1,5 @@
 class Calculator
-    def add(a, b, c=0)
-        a + b + c
-    end
+  def add(a, b, c=0)
+    a + b + c
+  end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,16 +1,15 @@
 require 'calculator'
 
 describe Calculator do
-    describe "#add" do
-      it "returns the sum of two numbers" do
-        calculator = Calculator.new
-        expect(calculator.add(5, 2)).to eql(7)
-      end
+  describe '#add' do
+    it 'returns the sum of two numbers' do
+      calculator = Calculator.new
+      expect(calculator.add(5, 2)).to eql(7)
     end
-
-    it "returns the sum of more than two numbers" do
-        calculator = Calculator.new
-        expect(calculator.add(2, 5, 7)).to eql(14)
-      end
-      
   end
+
+  it 'returns the sum of more than two numbers' do
+    calculator = Calculator.new
+    expect(calculator.add(2, 5, 7)).to eql(14)
+  end
+end
