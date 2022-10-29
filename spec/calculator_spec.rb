c765d8aa546c166b19c480c340e37f9b550525d1
Bas Man <bas.man.developer@gmail.com>
2022-10-29 16:34:33 +0100

    refactor

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -6,21 +6,24 @@ describe Calculator do
     it 'returns the sum of two numbers' do
       expect(calculator.add(5, 2)).to eq(7)
     end
-  end
 
-  it 'returns the sum of more than two numbers' do
-    expect(calculator.add(2, 5, 7)).to eq(14)
+    it 'returns the sum of more than two numbers' do
+      expect(calculator.add(2, 5, 7)).to eq(14)
+    end
   end
+  
   describe '#multiply' do
     it 'returns two numbes multiplied' do
       expect(calculator.multiply(3, 4)).to eq(12)
     end
   end
+  
   describe '#multiply' do
     it 'returns the first number minus the second number' do
       expect(calculator.subtract(5, 2)).to eq(3)
     end
   end
+  
   describe '#divide' do
     it 'returns the first number divided by the second' do
       expect(calculator.divide(12, 4)).to eq(3)
